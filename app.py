39c4b5acef15cc981eb7233a26d89015deb6d4fd
Julien de Carufel <jcarufel@cognibox.com>
2021-12-21 10:48:03 -0500

    i give up

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, request
+from dataclasses import dataclass
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+# @app.route('/public/<path:path>')
+# def public():
+#     return send_from_directory('js', path)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@dataclass
+class Image:
+    src: str
+
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template('index.html', images=[Image('https://via.placeholder.com/500'),Image('https://via.placeholder.com/500'),])
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
